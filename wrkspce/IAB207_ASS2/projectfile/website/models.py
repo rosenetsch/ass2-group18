3b8db5b5939686ef1dfd46dc64d8ca1f7a91026f
Rose Cross Netsch <n10796649@qut.edu.au>
2026-05-02 17:03:10 -0500

    Add Event model with various fields
    Event model no includes: Event.id, title, description, date, category, status, acknowledgement, user_id (of the creator)

--- wrkspce/IAB207_ASS2/projectfile/website/models.py
+++ wrkspce/IAB207_ASS2/projectfile/website/models.py
@@ -3,14 +3,23 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-   id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(200))
     Phone = db.Column(db.String(20))
     address = db.Column(db.String(200))
+
 class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    date = db.Column(db.DateTime)
+    category = db.Column(db.String(50))
+    status = db.Column(db.String(20))  # Open, Sold Out, Cancelled, Inactive
+    acknowledgement = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class Comment(db.Model):
 
