9ff7e1d571ca2690b6cf1486b317dfa57d54efa7
Rose Cross Netsch <n10796649@qut.edu.au>
2026-05-02 16:55:50 -0500

    Define User model with attributes
    Added attributes to User model including id, first_name, last_name, email, password, Phone, and address.

--- wrkspce/IAB207_ASS2/projectfile/website/models.py
+++ wrkspce/IAB207_ASS2/projectfile/website/models.py
@@ -3,7 +3,13 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-
+   id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(200))
+    Phone = db.Column(db.String(20))
+    address = db.Column(db.String(200))
 class Event(db.Model):
 
 class Comment(db.Model):
