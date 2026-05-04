8dbb33596b31b9397df7f584fc7b72c903b131c7
Lovern <lovernspam@gmail.com>
2026-05-04 20:27:07 +1000

    input all of loverns code

--- wrkspce/IAB207_ASS2/ass2-group18/livebeat/views.py
+++ wrkspce/IAB207_ASS2/ass2-group18/livebeat/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return '<h1>Starter code for assignment 3<h1>'
+    return render_template('index.html')
+
+
+@main_bp.route('/events')
+def event_details():
+    return render_template('event-details.html')
+
+
+@main_bp.route('/create-event')
+def create_event():
+    return render_template('create-event.html')
+
+
+@main_bp.route('/booking-history')
+def booking_history():
+    return render_template('booking-history.html')
