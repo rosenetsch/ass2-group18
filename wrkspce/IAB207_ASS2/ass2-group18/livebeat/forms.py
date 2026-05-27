d9a647530766b63a901cb624aba11bcd3efbab40
n11621249 <anthony.bearzatto@connect.qut.edu.au>
2026-05-27 21:10:24 +1000

    Change forms.py for improved structure

--- wrkspce/IAB207_ASS2/ass2-group18/livebeat/forms.py
+++ wrkspce/IAB207_ASS2/ass2-group18/livebeat/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms.fields import SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo, Regexp, Optional
-from wtforms.fields import TextAreaField, SelectField, IntegerField, FloatField, DateField, TimeField
-from wtforms.validators import NumberRange
-from wtforms.validators import ValidationError
+from flask_wtf.file import (
+    FileField,
+    FileAllowed
+)
+
 from wtforms.fields import (
     SubmitField,
     StringField,
@@ -30,142 +29,396 @@ from wtforms.validators import (
 
 from datetime import date
 
-class EventForm(FlaskForm):
-    title = StringField("Event Title", validators=[InputRequired()])
-    artist = StringField("Artist/Performer", validators=[InputRequired()])
-    description = TextAreaField("Event Description", validators=[InputRequired()])
 
-    category = SelectField(
-        "Genre",
-        choices=[
-            ("rock", "Rock"),
-            ("pop", "Pop"),
-            ("jazz", "Jazz"),
-            ("rnb", "RnB"),
-            ("soul", "Soul"),
-            ("other", "Other"),
-        ],
-        validators=[InputRequired()],
+# =========================================
+# EVENT FORM
+# =========================================
+
+class EventForm(FlaskForm):
+
+    title = StringField(
+
+        "Event Title",
+
+        validators=[
+            InputRequired()
+        ]
     )
 
-    ticket_type = SelectField(
-        "Ticket Type",
+    artist = StringField(
+
+        "Artist/Performer",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    description = TextAreaField(
+
+        "Event Description",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    category = SelectField(
+
+        "Genre",
+
         choices=[
-            ("standard", "Standard"),
-            ("vip", "VIP"),
-            ("premium", "Premium"),
+
+            ("rock", "Rock"),
+
+            ("pop", "Pop"),
+
+            ("jazz", "Jazz"),
+
+            ("rnb", "RnB"),
+
+            ("soul", "Soul"),
+
+            ("other", "Other"),
         ],
-        validators=[InputRequired()],
+
+        validators=[
+            InputRequired()
+        ],
     )
 
     capacity = SelectField(
+
         "Capacity",
+
         choices=[
+
             ("500", "500"),
+
             ("1000", "1000"),
+
             ("2000", "2000"),
+
             ("3000", "3000"),
+
             ("4000", "4000"),
+
             ("5000", "5000"),
         ],
-        validators=[InputRequired()],
-    )
-    date = DateField("Event Date", validators=[InputRequired()])
-    time = TimeField("Event Start Time", validators=[InputRequired()])
-    end_time = TimeField("Event End Time", validators=[Optional()])
 
-    venue_name = StringField("Venue Name", validators=[InputRequired()])
-    venue_address = StringField("Venue Address", validators=[InputRequired()])
+        validators=[
+            InputRequired()
+        ],
+    )
+
+    date = DateField(
+
+        "Event Date",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    # START TIME
+
+    time = TimeField(
+
+        "Event Start Time",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    # END TIME
+
+    end_time = TimeField(
+
+        "Event End Time"
+    )
+
+    venue_name = StringField(
+
+        "Venue Name",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    venue_address = StringField(
+
+        "Venue Address",
+
+        validators=[
+            InputRequired()
+        ]
+    )
+
+    # STANDARD PRICE
 
     standard_price = FloatField(
+
         "Standard Ticket Price (AUD $)",
+
         validators=[
+
             InputRequired(),
+
             NumberRange(min=0)
         ]
     )
+
+    # VIP PRICE
 
     vip_price = FloatField(
+
         "VIP Ticket Price (AUD $)",
+
         validators=[
+
             InputRequired(),
+
             NumberRange(min=0)
         ]
     )
+
+    # PREMIUM PRICE
 
     premium_price = FloatField(
+
         "Premium Ticket Price (AUD $)",
+
         validators=[
+
             InputRequired(),
+
             NumberRange(min=0)
         ]
     )
 
-    image = FileField("Event Image", validators=[Optional(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only.')])
+    # IMAGE
 
-    acknowledgement = SelectField(
-        "Acknowledgement of Country",
-        choices=[
-            ("none", "No Acknowledgement of Country"),
-            ("generic", "Acknowledgement of Country: generic"),
-            ("enhanced", "Acknowledgement of Country: enhanced"),
-        ],
-        validators=[InputRequired()],
+    image = FileField(
+
+        "Event Image",
+
+        validators=[
+
+            Optional(),
+
+            FileAllowed(
+                ["jpg", "jpeg", "png", "gif"],
+                "Images only."
+            )
+        ]
     )
 
-    submit = SubmitField("Save Event")
+    acknowledgement = SelectField(
+
+        "Acknowledgement of Country",
+
+        choices=[
+
+            ("none", "No Acknowledgement of Country"),
+
+            ("generic", "Generic"),
+
+            ("enhanced", "Enhanced"),
+        ],
+
+        validators=[
+            InputRequired()
+        ],
+    )
+
+    submit = SubmitField(
+        "Save Event"
+    )
+
+    # DATE VALIDATION
+
     def validate_date(self, field):
 
-    if field.data < date.today():
+        if field.data < date.today():
 
-        raise ValidationError(
-            "Event date cannot be in the past."
-        )
+            raise ValidationError(
+                "Event date cannot be in the past."
+            )
 
-# creates the login information
+
+# =========================================
+# LOGIN FORM
+# =========================================
+
 class LoginForm(FlaskForm):
+
     email = StringField(
+
         "Email Address",
+
         validators=[
-            InputRequired("Enter email address"),
-            Email("Please enter a valid email")
+
+            InputRequired(
+                "Enter email address"
+            ),
+
+            Email(
+                "Please enter a valid email"
+            )
         ]
     )
 
     password = PasswordField(
+
         "Password",
-        validators=[InputRequired("Enter your password")]
+
+        validators=[
+
+            InputRequired(
+                "Enter your password"
+            )
+        ]
     )
 
-    submit = SubmitField("Login")
+    submit = SubmitField(
+        "Login"
+    )
 
- # this is the registration form
+
+# =========================================
+# REGISTER FORM
+# =========================================
+
 class RegisterForm(FlaskForm):
-    first_name = StringField("First Name", validators=[InputRequired("Enter first name")])
-    last_name = StringField("Surname", validators=[InputRequired("Enter surname")])
-    email = StringField(
-        "Email Address",
-        validators=[InputRequired("Enter email address"), Email("Please enter a valid email")],
+
+    first_name = StringField(
+
+        "First Name",
+
+        validators=[
+
+            InputRequired(
+                "Enter first name"
+            )
+        ]
     )
-    contact_number = StringField("Contact Number", validators=[InputRequired("Enter contact number")])
-    street_address = StringField("Street Address", validators=[InputRequired("Enter street address")])
-    # password should be equal to the data entered in confirm
-    password=PasswordField("Password", validators=[
-                  InputRequired(),
-                  Length(min=8, message="Password must be at least 8 characters long"),
-                  Regexp(r"^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).+$", message="Password must include at least 1 uppercase letter and 1 symbol"),
-                  EqualTo('confirm', message="Passwords should match")
-                  ])
-    confirm = PasswordField("Confirm Password", validators=[InputRequired("Confirm your password")])
 
-    # submit button
-    submit = SubmitField("Register")
+    last_name = StringField(
 
+        "Surname",
+
+        validators=[
+
+            InputRequired(
+                "Enter surname"
+            )
+        ]
+    )
+
+    email = StringField(
+
+        "Email Address",
+
+        validators=[
+
+            InputRequired(
+                "Enter email address"
+            ),
+
+            Email(
+                "Please enter a valid email"
+            ),
+        ],
+    )
+
+    contact_number = StringField(
+
+        "Contact Number",
+
+        validators=[
+
+            InputRequired(
+                "Enter contact number"
+            )
+        ]
+    )
+
+    street_address = StringField(
+
+        "Street Address",
+
+        validators=[
+
+            InputRequired(
+                "Enter street address"
+            )
+        ]
+    )
+
+    password = PasswordField(
+
+        "Password",
+
+        validators=[
+
+            InputRequired(),
+
+            Length(
+                min=8,
+                message="Password must be at least 8 characters long"
+            ),
+
+            Regexp(
+                r"^(?=.*[A-Z])(?=.*[^A-Za-z0-9]).+$",
+                message="Password must include at least 1 uppercase letter and 1 symbol"
+            ),
+
+            EqualTo(
+                'confirm',
+                message="Passwords should match"
+            )
+        ]
+    )
+
+    confirm = PasswordField(
+
+        "Confirm Password",
+
+        validators=[
+
+            InputRequired(
+                "Confirm your password"
+            )
+        ]
+    )
+
+    submit = SubmitField(
+        "Register"
+    )
+
+
+# =========================================
+# BOOKING FORM
+# =========================================
 
 class BookingForm(FlaskForm):
+
     ticket_quantity = IntegerField(
-        "Ticket quantity",
-        validators=[InputRequired(), NumberRange(min=1)],
+
+        "Ticket Quantity",
+
+        validators=[
+
+            InputRequired(),
+
+            NumberRange(min=1)
+        ],
+
         default=1,
     )
-    submit = SubmitField("Book Now")
+
+    submit = SubmitField(
+        "Book Now"
+    )
